fc14b11183299f3d95722d50b1779eb8fdadd603
hugodlrll <hugo.del49@gmail.com>
2025-10-16 09:31:23 +0200

    1.3 part finished
    Signed-off-by: hugodlrll <hugo.del49@gmail.com>

--- OpenCV-calibration-part1.py
+++ OpenCV-calibration-part1.py
@@ -16,45 +16,33 @@ def openCAM():
             cam = cv.VideoCapture(int(cam_number))
     return cam
 
-def cleanup(window1, window2, cap):
-    cv.destroyWindow(window1)
-    cv.destroyWindow(window2)
-    cap.release()
+def cleanup(window):
+    cv.destroyWindow(window)
 
 def main():
-    # Open the camera device
-    cap = openCAM()
+    path = "../calib_gopro/calib_gopro/GOPR84"
     # Creating the windows to display the video stream
-    window1 = "Gray image"
-    cv.namedWindow(window1, cv.WINDOW_AUTOSIZE)
-    window2 = "Coloured image"
-    cv.namedWindow(window2, cv.WINDOW_AUTOSIZE)
+    window = "Gopro calib"
+    cv.namedWindow(window, cv.WINDOW_AUTOSIZE)
     # Boolean value to change between gray and coloured mode
     isGray =  True
     while True:
         # Capture frame-by-frame
-        ret, image = cap.read()
-        # if frame is read correctly ret is True
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
-        # if g is pressed, change the color mode of win1
-        if cv.waitKey(1) == ord('g'):
-            isGray = not isGray
-        # Our operations on the frame come here
-        gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-        # Display the resulting frame for win 1(coloured or gray depending if g key has been pressed)
-        if isGray:
-            cv.imshow(window1, gray_image)
-        else:
-            cv.imshow(window1, image) 
-        # Display the resulting frame for win 2
-        cv.imshow(window2, image)
-        
-        # When esc is pressed, exit the loop
-        if cv.waitKey(1) == ESC_KEY:
-            cleanup(window1, window2, cap)
-            break
+        for i in range(1, 27):
+            # Format the path correctly with zero-padding
+            path = f"../calib_gopro/calib_gopro/GOPR84{i:02d}.JPG"
+            image = cv.imread(path)
+            
+            # Check if image was loaded successfully
+            if image is None:
+                print(f"Error: Could not load image {path}")
+                return
+                
+            cv.imshow(window, image)
+            # When esc is pressed, exit the loop
+            if cv.waitKey(1) == ESC_KEY:
+                cleanup(window)
+                return
 
 # Starting the code
 if __name__ == "__main__":
